3944ba8daa6cab80f4bca1380264329540ac864b
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-10-29 18:53:12 +0100

    Update scheme results

--- app/graphql/search-customer.js
+++ app/graphql/search-customer.js
@@ -4,19 +4,25 @@ const { gql } = require('graphql-request')
 const query = gql`
 query searchCustomers($searchString: String!, $searchFields: [String!]) {
   searchCustomers(searchString: $searchString, searchFields: $searchFields) {
-    sbi
-    firstname
-    lastname
-    address
-    email
-    phone
+    searchResults {
+      sbi
+      firstname
+      lastname
+      address
+      email
+      phone
+      schemes {
+        name
+        reference
+      }
+    }
   }
 }
 `
 
 const searchCustomer = async (searchString, searchFields) => {
   const customers = await client.request(query, { searchString, searchFields })
-  return customers?.searchCustomers
+  return customers?.searchCustomers?.searchResults
 }
 
 module.exports = searchCustomer
--- app/routes/search-customers.js
+++ app/routes/search-customers.js
@@ -18,7 +18,18 @@ module.exports = [{
       const { searchString, searchField } = h.request.payload
 
       searchField !== 'all' && stringFields.push(searchField)
-      const customers = await searchCustomer(searchString, stringFields)
+      const customerSearchResults = await searchCustomer(searchString, stringFields)
+      const customers = customerSearchResults.map(customer => {
+        const schemes = customer?.schemes?.map(scheme => scheme.name.toUpperCase())
+        return {
+          sbi: customer.sbi,
+          name: `${customer.firstname} ${customer.lastname}`,
+          address: customer.address,
+          email: customer.email,
+          phone: customer.phone,
+          schemes
+        }
+      })
       return h.view('search-customers', { customers, searchString, searchField })
     }
   }
